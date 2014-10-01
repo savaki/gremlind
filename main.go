07b818aada50db01aff5a387f15f8e5188a83291
savaki <matt.ho@gmail.com>
2014-10-01 03:01:17 -0700

    - added pub/sub model for messaging

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/codegangsta/cli"
+	"github.com/savaki/broadcast"
 	"github.com/savaki/gremlind/executor"
 	"github.com/savaki/gremlind/manifest"
 	"log"
@@ -9,7 +10,8 @@ import (
 )
 
 const (
-	FieldManifest = "manifest"
+	FieldManifest    = "manifest"
+	FieldLoggingPort = "logging-port"
 )
 
 func main() {
@@ -18,9 +20,19 @@ func main() {
 	app.Usage = "gremlin daemon to run inside a container"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{FieldManifest, "/etc/gremlin/gremlin.hcl", "the name of the gremlin manifest file", "GREMLIN_MANIFEST"},
+		cli.IntFlag{FieldLoggingPort, 0, "the port to listen to for the firehost; speak websocket client", "GREMLIN_LOGGING_PORT"},
 	}
 	app.Action = Run
 	app.Run(os.Args)
+}
+
+type ChanWriter struct {
+	messages chan []byte
+}
+
+func (c *ChanWriter) Write(p []byte) (n int, err error) {
+	c.messages <- p
+	return len(p), nil
 }
 
 func Run(c *cli.Context) {
@@ -30,8 +42,25 @@ func Run(c *cli.Context) {
 		log.Fatalln(err)
 	}
 
+	publishers := map[string]broadcast.Publisher{}
 	for id, program := range m.Program {
-		e := executor.New(id, program, os.Stdout, os.Stderr)
+		messages := make(chan []byte, 4096)
+		publisher := broadcast.New((<-chan []byte)(messages))
+		publisher.Start()
+		publishers[id] = publisher
+
+		response := make(chan *broadcast.Subscription, 1)
+		publisher.Subscribe(response)
+		subscription := <-response
+		go func() {
+			for {
+				data := <-subscription.Receive
+				os.Stdout.Write(data)
+			}
+		}()
+
+		writer := &ChanWriter{messages: messages}
+		e := executor.New(id, program, writer, writer)
 		e.Run()
 	}
 
